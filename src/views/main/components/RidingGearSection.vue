d3cb5bea618828caf462e66863554e744b30c4a7
Sean <joajoa70584@gmail.com>
2025-03-04 09:21:30 +0900

    feat: 메인 페이지 퍼블리싱

---
+++ src/views/main/components/RidingGearSection.vue
@@ -0,0 +1,59 @@
+<script setup></script>
+<template>
+  <article class="flex flex-col gap-[50px]">
+    <div class="flex flex-col text-center gap-4">
+      <h3 class="text-3xl text-primaryRed font-bold">Rider Gear</h3>
+      <h2 class="text-4xl font-bold dark:text-black1">당신의 라이딩을 위한 완벽한 선택</h2>
+    </div>
+    <div class="grid grid-cols-10">
+      <!-- 첫번째 컬럼 -->
+      <div class="col-span-2 border-r pl-4 border-black4 dark:border-black4">
+        <div class="border-b border-black4 pr-4 pb-4">
+          <img src="/main/main_riding_gear_1.png" alt="riding gear" class="w-full object-cover" />
+        </div>
+        <span class="block text-sub-title pt-3 pr-2 truncate dark:text-black1"
+          >락브로스 쿨 마스크 블랙 WB-003</span
+        >
+      </div>
+      <!-- 두번째 컬럼 -->
+      <div class="col-span-2 pl-4 pt-5">
+        <span
+          class="block text-sub-title pb-3 pr-2 border-r border-black4 dark:border-black4 dark:text-black1 truncate"
+          >락브로스 방한 자전거장갑 S091-2</span
+        >
+        <div class="h-[340px] pr-4 pt-5 border-r border-t border-black4 dark:border-black4">
+          <img src="/main/main_riding_gear_2.png" alt="riding gear" class="w-full object-cover" />
+        </div>
+      </div>
+      <!-- 세번째 컬럼 -->
+      <div class="col-span-2 border-r pl-4 border-black4 dark:border-black4">
+        <div class="border-b border-black4 pr-4 pb-4">
+          <img src="/main/main_riding_gear_3.png" alt="riding gear" class="w-full object-cover" />
+        </div>
+        <span class="block text-sub-title pt-3 pr-2 truncate dark:text-black1"
+          >레빗 하이퍼스피드 레이싱 슈트</span
+        >
+      </div>
+
+      <!-- 네번째 컬럼 -->
+      <div class="col-span-2 pl-4 pt-5">
+        <span
+          class="block text-sub-title pb-3 pr-2 border-r border-black4 dark:border-black4 dark:text-black1 truncate"
+          >노스웨이브 엑스 셀시우스 아틱 GTX</span
+        >
+        <div class="h-[340px] pr-4 pt-5 border-r border-t border-black4 dark:border-black4">
+          <img src="/main/main_riding_gear_4.png" alt="riding gear" class="w-full object-cover" />
+        </div>
+      </div>
+      <!-- 다섯번째 컬럼 -->
+      <div class="col-span-2 border-r pl-4 border-black4 dark:border-black4">
+        <div class="border-b border-black4 pr-4 pb-4">
+          <img src="/main/main_riding_gear_5.png" alt="riding gear" class="w-full object-cover" />
+        </div>
+        <span class="block text-sub-title pt-3 pr-2 truncate dark:text-black1"
+          >락브로스 어반 캐주얼 헬멧 그레이 WT-09</span
+        >
+      </div>
+    </div>
+  </article>
+</template>
