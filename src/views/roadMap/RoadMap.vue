4aff5feb416d7dbe8ea86cfc3c32828b60f120ed
chaeee1 <qocodus0212@gmail.com>
2025-03-05 12:40:09 +0900

    feat. 편의시설 마커 기능 구현 완료

--- src/views/roadMap/RoadMap.vue
+++ src/views/roadMap/RoadMap.vue
@@ -1,17 +1,26 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import MapHeader from "./MapHeader.vue";
-import L from "leaflet";
 import { fetchMap } from "./api/fetchMap";
 import { fetchPlaces } from "./api/fetchPlaces";
-import MapModal from "./MapModal.vue"; 
+import { fetchConvenienceFacilities } from "./api/fetchConvenience";
+import MapModal from "./MapModal.vue";
 
 const mapContainer = ref(null);
 const map = ref(null);
 const userLatLng = ref(null);
-const isMarkersVisible = ref(false);
 const selectedStation = ref(null);
-const isToggled = ref(false);
+const selectedFacility = ref(null);
+const bikeMarkers = ref([]);
+const airPumpMarkers = ref([]);
+const rackMarkers = ref([]);
+const repairMarkers = ref([]);
+const isToggled = ref({
+  bikeStations: false,
+  airPumps: false,
+  racks: false,
+  repairShops: false
+});
 
 onMounted(() => {
   if (mapContainer.value) {
@@ -23,21 +32,57 @@ onMounted(() => {
   }
 });
 
-const toggleBikeStations = () => {
-  isToggled.value = !isToggled.value;
-  if (map.value) {
-    if (!isMarkersVisible.value) {
-      fetchPlaces(map.value, userLatLng.value, (stationData) => {
-        selectedStation.value = stationData;
-      });
-    } else {
-      map.value.eachLayer((layer) => {
-        if (layer instanceof L.Marker) {
-          map.value.removeLayer(layer);
-        }
-      });
+
+const toggleBikeStations = async () => {
+  if (!map.value) return;
+
+  if (!isToggled.value.bikeStations) {
+    if (bikeMarkers.value.length > 0) {
+      bikeMarkers.value.forEach(marker => map.value.removeLayer(marker));
+      bikeMarkers.value = [];
     }
-    isMarkersVisible.value = !isMarkersVisible.value;
+
+    const newMarkers = await fetchPlaces(map.value, userLatLng.value, (stationData) => {
+      selectedStation.value = stationData;
+    });
+
+    bikeMarkers.value = newMarkers || [];
+  } else {
+    bikeMarkers.value.forEach(marker => map.value.removeLayer(marker));
+    bikeMarkers.value = [];
+  }
+
+  isToggled.value.bikeStations = !isToggled.value.bikeStations;
+};
+
+const toggleConvenienceFacilities = async (type) => {
+  if (map.value) {
+    let targetMarkers = null;
+    let toggleKey = null;
+
+    if (type === "공기주입기") {
+      targetMarkers = airPumpMarkers;
+      toggleKey = "airPumps";
+    } else if (type === "보관소") {
+      targetMarkers = rackMarkers;
+      toggleKey = "racks";
+    } else if (type === "수리시설") {
+      targetMarkers = repairMarkers;
+      toggleKey = "repairShops";
+    }
+
+    if (!isToggled.value[toggleKey]) {
+      const newMarkers = await fetchConvenienceFacilities(map.value, userLatLng.value, type, (facilityData) => {
+        selectedFacility.value = facilityData;
+      });
+
+      targetMarkers.value = newMarkers;
+    } else {
+
+      targetMarkers.value.forEach(marker => map.value.removeLayer(marker));
+      targetMarkers.value = [];
+    }
+    isToggled.value[toggleKey] = !isToggled.value[toggleKey];
   }
 };
 </script>
@@ -47,24 +92,24 @@ const toggleBikeStations = () => {
     <div ref="mapContainer" class="absolute w-full h-full z-0"></div>
     <MapHeader />
 
-
     <MapModal
-      v-if="selectedStation"
-      :stationName="selectedStation.stationName"
-      :availableBikes="selectedStation.availableBikes"
-      :address="selectedStation.address"
-      :facilityType="selectedStation.facilityType"
-      :operatingDays="selectedStation.operatingDays"
+      v-if="selectedStation || selectedFacility"
+      :stationName="selectedStation?.stationName || selectedFacility?.name"
+      :availableBikes="selectedStation?.availableBikes"
+      :address="selectedStation?.address || selectedFacility?.address"
+      :facilityType="selectedFacility?.type"
+      :operatingDays="selectedStation?.operatingDays"
     />
+
 
     <div class="relative flex top-[2.5%] w-full h-[42px] text-lg font-medium" >
   <div class="ml-auto flex gap-4 max-w-[600px] flex-shrink-0 justify-end pr-8">
 
- <button 
+    <button 
   @click="toggleBikeStations"
   :class="{
-    'bg-white dark:text-black1 dark:bg-black9 dark:hover:bg-primaryRed hover:bg-primaryRed': !isToggled,
-    'bg-primaryRed text-white dark:bg-primaryRed dark:text-white': isToggled
+    'bg-white dark:text-black1 dark:bg-black9 dark:hover:bg-primaryRed hover:bg-primaryRed': !isToggled.bikeStations,
+    'bg-primaryRed text-white dark:bg-primaryRed dark:text-white': isToggled.bikeStations
   }"
   class="flex items-center justify-center rounded-3xl gap-2 shadow-lg hover:scale-110 transition-transform duration-200"
 >
@@ -78,7 +123,15 @@ const toggleBikeStations = () => {
 </defs>
 </svg>
 공공자전거</button>
-<button class="bg-white flex items-center justify-center rounded-3xl gap-2 shadow-lg hover:scale-110 transition-transform duration-200 dark:text-black1 dark:bg-black9 dark:hover:bg-primaryRed hover:bg-primaryRed">
+
+<button 
+  @click="toggleConvenienceFacilities('수리시설')"
+  :class="{
+    'bg-white text-black dark:text-black1 dark:bg-black9 dark:hover:bg-primaryRed hover:bg-primaryRed': !isToggled.repairShops,
+    'bg-primaryRed text-white dark:bg-primaryRed dark:text-white': isToggled.repairShops
+  }"
+  class="flex items-center justify-center rounded-3xl gap-2 shadow-lg hover:scale-110 transition-transform duration-200"
+>
   <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 <rect width="20" height="20" fill="url(#pattern0_273_500)"/>
 <defs>
@@ -89,7 +142,15 @@ const toggleBikeStations = () => {
 </defs>
 </svg>
 수리시설</button>
-<button class="bg-white flex items-center justify-center rounded-3xl gap-2 shadow-lg hover:scale-110 transition-transform duration-200 dark:text-black1 dark:bg-black9 dark:hover:bg-primaryRed hover:bg-primaryRed">
+
+<button 
+  @click="toggleConvenienceFacilities('공기주입기')"
+  :class="{
+    'bg-white text-black dark:text-black1 dark:bg-black9 dark:hover:bg-primaryRed hover:bg-primaryRed': !isToggled.airPumps,
+    'bg-primaryRed text-white dark:bg-primaryRed dark:text-white': isToggled.airPumps
+  }"
+  class="flex items-center justify-center rounded-3xl gap-2 shadow-lg hover:scale-110 transition-transform duration-200"
+>
   <svg width="20" height="22" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 <rect width="20" height="22" fill="url(#pattern0_273_503)"/>
 <defs>
@@ -100,7 +161,14 @@ const toggleBikeStations = () => {
 </defs>
 </svg>
 공기주입기</button>
-<button class="bg-white flex items-center justify-center rounded-3xl gap-2 shadow-lg hover:scale-110 transition-transform duration-200 dark:text-black1 dark:bg-black9 dark:hover:bg-primaryRed hover:bg-primaryRed"><svg width="23" height="24" viewBox="0 0 23 24" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<button 
+  @click="toggleConvenienceFacilities('보관소')"
+  :class="{
+    'bg-white text-black dark:text-black1 dark:bg-black9 dark:hover:bg-primaryRed hover:bg-primaryRed': !isToggled.racks,
+    'bg-primaryRed text-white dark:bg-primaryRed dark:text-white': isToggled.racks
+  }"
+  class="flex items-center justify-center rounded-3xl gap-2 shadow-lg hover:scale-110 transition-transform duration-200"
+><svg width="23" height="24" viewBox="0 0 23 24" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 <rect y="0.5" width="23" height="23" fill="url(#pattern0_273_506)"/>
 <defs>
 <pattern id="pattern0_273_506" patternContentUnits="objectBoundingBox" width="1" height="1">
