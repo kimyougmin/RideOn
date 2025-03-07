62325e58bd50a4683515655de8e1eb4e0247ac67
Sean <joajoa70584@gmail.com>
2025-03-07 16:05:53 +0900

    style: 커서 포인터 추가

--- src/views/riderParts/components/GearCategoriesSection.vue
+++ src/views/riderParts/components/GearCategoriesSection.vue
@@ -1,15 +1,15 @@
 <script setup>
-import { useRouter } from 'vue-router';
+import { useRouter } from 'vue-router'
 
-const router = useRouter();
+const router = useRouter()
 
 const goToSearch = (query) => {
-  if(!query) return;
+  if (!query) return
 
   router.push({
     path: '/riderPartsSearch',
     query: { keyword: query },
-  });
+  })
 }
 </script>
 
@@ -17,27 +17,45 @@ const goToSearch = (query) => {
   <article class="w-full flex flex-col gap-8">
     <p class="text-primaryRed text-title font-bold">Gear Categories</p>
     <div class="w-full grid grid-cols-6 gap-4">
-      <div class="flex flex-col items-center gap-5" @click="goToSearch('자전거헬멧')">
+      <div
+        class="flex flex-col items-center gap-5 cursor-pointer"
+        @click="goToSearch('자전거헬멧')"
+      >
         <img src="/riderPageImage/category_helmat.png" />
         <p class="text-sub-title font-bold m-0 dark:text-black1">헬멧</p>
       </div>
-      <div class="flex flex-col items-center gap-5" @click="goToSearch('자전거라이트')">
+      <div
+        class="flex flex-col items-center gap-5 cursor-pointer"
+        @click="goToSearch('자전거라이트')"
+      >
         <img src="/riderPageImage/category_light.png" />
         <p class="text-sub-title font-bold m-0 dark:text-black1">라이트</p>
       </div>
-      <div class="flex flex-col items-center gap-5" @click="goToSearch('자전거물통+자전거물통케이지')">
+      <div
+        class="flex flex-col items-center gap-5 cursor-pointer"
+        @click="goToSearch('자전거물통+자전거물통케이지')"
+      >
         <img src="/riderPageImage/category_waterBottle.png" />
         <p class="text-sub-title font-bold m-0 dark:text-black1">물통 & 물통케이지</p>
       </div>
-      <div class="flex flex-col items-center gap-5" @click="goToSearch('자전거자물쇠')">
+      <div
+        class="flex flex-col items-center gap-5 cursor-pointer"
+        @click="goToSearch('자전거자물쇠')"
+      >
         <img src="/riderPageImage/category_lock.png" />
         <p class="text-sub-title font-bold m-0 dark:text-black1">자물쇠</p>
       </div>
-      <div class="flex flex-col items-center gap-5" @click="goToSearch('자전거휴대폰거치대')">
+      <div
+        class="flex flex-col items-center gap-5 cursor-pointer"
+        @click="goToSearch('자전거휴대폰거치대')"
+      >
         <img src="/riderPageImage/category_phoneStand.png" />
         <p class="text-sub-title font-bold m-0 dark:text-black1">휴대폰거치대</p>
       </div>
-      <div class="flex flex-col items-center gap-5" @click="goToSearch('자전거펌프')">
+      <div
+        class="flex flex-col items-center gap-5 cursor-pointer"
+        @click="goToSearch('자전거펌프')"
+      >
         <img src="/riderPageImage/category_pump.png" />
         <p class="text-sub-title font-bold m-0 dark:text-black1">펌프</p>
       </div>
