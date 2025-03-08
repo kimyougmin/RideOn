38cc4ffd40d95e34311c5837878ecbeb7fc6a1be
Sean <joajoa70584@gmail.com>
2025-03-08 18:05:46 +0900

    feat: naver api 프록시 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -6,24 +6,26 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
     proxy: {
-      "/naver-api": {
-        target: "https://openapi.naver.com",
+      '/naver-api': {
+        target: 'https://openapi.naver.com',
         changeOrigin: true,
         secure: true,
-        rewrite: (path) => path.replace(/^\/naver-api/, ""),
+        rewrite: (path) => path.replace(/^\/naver-api/, ''),
+      },
+      '/v1': {
+        target: 'https://openapi.naver.com',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
       },
     },
   },
-
 })
