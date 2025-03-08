c2910e6e14fd9efe5e09583347fcf46f22f47bff
Sean <joajoa70584@gmail.com>
2025-03-08 10:17:40 +0900

    fix: 필터 상태명 오타 수정

--- src/views/qnaBoard/components/QnaFilterMenu.vue
+++ src/views/qnaBoard/components/QnaFilterMenu.vue
@@ -9,15 +9,15 @@
     </span>
     <span
       class="text-sub-title dark:text-black1 cursor-pointer"
-      :class="{ 'font-bold text-primaryRed dark:text-primaryRed': selectedFilter === 'solved' }"
-      @click="emit('update:modelValue', 'solved')"
+      :class="{ 'font-bold text-primaryRed dark:text-primaryRed': selectedFilter === 'SOLVED' }"
+      @click="emit('update:modelValue', 'SOLVED')"
     >
       해결
     </span>
     <span
       class="text-sub-title dark:text-black1 cursor-pointer"
-      :class="{ 'font-bold text-primaryRed dark:text-primaryRed': selectedFilter === 'unsolved' }"
-      @click="emit('update:modelValue', 'unsolved')"
+      :class="{ 'font-bold text-primaryRed dark:text-primaryRed': selectedFilter === 'UNSOLVED' }"
+      @click="emit('update:modelValue', 'UNSOLVED')"
     >
       미해결
     </span>
